f9c617ab5b421c6dffc64798ee1bb2b90619e99c
matthewdicataldo <matthewdicataldo@gmail.com>
2023-01-14 16:26:16 -0700

    add test page for editor

---
+++ src/routes/(container)/editor/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	// import type { PageData } from './$types';
+
+	// export let data: PageData;
+</script>
+
+<input type="text" id="entry-title" value="Title" />
+
+<style lang="postcss">
+</style>
