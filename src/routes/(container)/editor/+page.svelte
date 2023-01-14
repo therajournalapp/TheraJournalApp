331cbb422f9023686d780532bb2e3a0635c255d0
matthewdicataldo <matthewdicataldo@gmail.com>
2023-01-14 16:52:42 -0700

    copy example editor from docs

--- src/routes/(container)/editor/+page.svelte
+++ src/routes/(container)/editor/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	// import type { PageData } from './$types';
+	import Tiptap from '$lib/Tiptap.svelte';
 
 	// export let data: PageData;
 </script>
 
-<input type="text" id="entry-title" value="Title" />
+<!-- <input type="text" id="entry-title" value="Title" /> -->
+
+<Tiptap />
 
 <style lang="postcss">
 </style>
