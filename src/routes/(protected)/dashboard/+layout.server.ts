70e585dae102bd1937e9992cd7df815f1ab5d58b
matthewdicataldo <matthewdicataldo@gmail.com>
2023-01-31 14:09:48 -0700

    fix: change journalEntries to journal_entries

--- src/routes/(protected)/dashboard/+layout.server.ts
+++ src/routes/(protected)/dashboard/+layout.server.ts
@@ -3,28 +3,27 @@ import { PrismaClient, type JournalEntry } from '@prisma/client';
 const prisma = new PrismaClient()
 
 
-export const load = (async ({locals}) => {
+export const load = (async ({ locals }) => {
     const { session, user } = await locals.validateUser();
-    if(!session) return { error: 401, message: "Unauthorized" };
-    if(user)
-    {
-  
-    // const post_by_user : JournalEntry[] | null =  await prisma.user.findUnique({
-    //     where: {
-    //         id: user.userId
-    //     }}).JournalEntry();
-    const post_by_user : JournalEntry[] | null =  await prisma.journalEntry.findMany({
-        where: {
-            user_id: user.userId,     
-        },
-        include: {
-            SharedEntry: true,
-        }
-    });
-    console.log(post_by_user);
-    const journal_entries = post_by_user.map(entry => {
+    if (!session) return { error: 401, message: "Unauthorized" };
+    if (user) {
+
+        // const post_by_user : JournalEntry[] | null =  await prisma.user.findUnique({
+        //     where: {
+        //         id: user.userId
+        //     }}).JournalEntry();
+        const post_by_user: JournalEntry[] | null = await prisma.journalEntry.findMany({
+            where: {
+                user_id: user.userId,
+            },
+            include: {
+                SharedEntry: true,
+            }
+        });
+        console.log(post_by_user);
+        const journal_entries = post_by_user.map(entry => {
             const { SharedEntry, ...rest } = entry;
-            
+
             if (SharedEntry.length > 0) {
                 rest.shared = true;
             } else {
@@ -33,28 +32,28 @@ export const load = (async ({locals}) => {
 
             return rest
         });
-    console.log(journal_entries);
-    
-    const habits = await prisma.habit.findMany({
-        orderBy: {
-            id: 'desc'
-        },
-        include: {
-            HabitEntry: {
-                where: {
-                    date: {
-                        lte: new Date(),
-                        gte: new Date(new Date().setDate(new Date().getDate() - 7))
+        console.log(journal_entries);
+
+        const habits = await prisma.habit.findMany({
+            orderBy: {
+                id: 'desc'
+            },
+            include: {
+                HabitEntry: {
+                    where: {
+                        date: {
+                            lte: new Date(),
+                            gte: new Date(new Date().setDate(new Date().getDate() - 7))
+                        }
                     }
                 }
             }
-        }
-    });
-    
-    return {
-        entries: journalEntries,
-        habits: habits
-    };
-}
-return { error: 401, message: "Unauthorized" };
+        });
+
+        return {
+            entries: journal_entries,
+            habits: habits
+        };
+    }
+    return { error: 401, message: "Unauthorized" };
 }) satisfies LayoutServerLoad;
