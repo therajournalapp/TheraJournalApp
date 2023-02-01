3eb864e3bd41c0b46d475d368e8403ece2874b6e
matthewdicataldo <matthewdicataldo@gmail.com>
2023-02-01 15:14:43 -0700

    fix: height jumping on user add/remove

--- src/lib/components/ShareSelector.svelte
+++ src/lib/components/ShareSelector.svelte
@@ -8,23 +8,36 @@
 		TransitionChild
 	} from '@rgossiaux/svelte-headlessui';
 	import 'iconify-icon';
+	import { fade, fly } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 
 	// Used to set the title of the dialog, should be the same as the entry or habit title
 	export let title: string;
-
 	// Used to change the button size
 	export let big: boolean = true; //TODO: change back to false by default
 
 	// Used to track if the dialog is open or not
 	let isOpen = false;
-
 	// Used to show an error for the add user form
 	let error = false;
-
 	// Used for styling button
 	let hover = false;
 	let icon = 'ph:lock-key';
 	let color = '#808080';
+	$: {
+		if (share) {
+			icon = 'ph:users';
+			color = '#739244';
+		} else {
+			if (hover) {
+				icon = 'ph:lock-key-open';
+				color = '#5f7938';
+			} else {
+				icon = 'ph:lock-key';
+				color = '#808080';
+			}
+		}
+	}
 
 	// Handles the submit of the add user form
 	// takes in user email
@@ -37,6 +50,12 @@
 		if (shared_to.filter((u) => u.email === data.user).length > 0) {
 			error = true;
 			return;
+		}
+		if (shared_to.length == 0) {
+			shareAnim = true;
+			setTimeout(() => {
+				shareAnim = false;
+			}, 350);
 		}
 		shared_to.push({ email: data.user as string });
 		shared_to = shared_to;
@@ -86,10 +105,14 @@
 
 	// Used to track if the entry is shared or not
 	// Changes the icon of the button
+	// TODO: hook up to backend
 	let share = shared_to.length > 0 ? true : false;
 	$: {
 		share = shared_to.length > 0 ? true : false;
 	}
+
+	// Used to trigger animation when going from unshared to shared
+	let shareAnim = false;
 
 	// TODO: hook up to backend, maybe even preload on hover??
 	// Used to track if the dialog has been opened for the first time
@@ -135,13 +158,14 @@
 {#if big}
 	<button
 		class="btn-alt !px-3"
+		class:shared={shareAnim}
 		on:click={() => {
 			isOpen = true;
 		}}
 		on:mouseover={() => {
 			hover = true;
 		}}
-		on:mouseout={() => {
+		on:mouseleave={() => {
 			hover = false;
 		}}
 		on:focus={() => {
@@ -164,7 +188,7 @@
 {:else}
 	<button
 		class="toggle"
-		class:big
+		class:shared={shareAnim}
 		on:click={() => {
 			isOpen = true;
 		}}
@@ -175,10 +199,10 @@
 			hover = false;
 		}}
 		on:focus={() => {
-			hover = true;
+			// hover = true;
 		}}
 		on:blur={() => {
-			hover = false;
+			// hover = false;
 		}}
 	>
 		<iconify-icon inline {icon} class="text-[23px]" class:big-i={big} style="color: {color}" />
@@ -215,7 +239,7 @@
 				leaveTo="opacity-0 scale-95"
 			>
 				<div
-					class="pointer-events-auto h-fit max-h-[500px] w-[350px] rounded-lg bg-white p-5 shadow-xl"
+					class="pointer-events-auto h-[500px] w-[350px] rounded-lg bg-white p-5 shadow-xl transition-all"
 				>
 					<div class="flex h-full flex-col justify-between">
 						<div class="flex flex-col gap-3">
@@ -244,7 +268,7 @@
 							<span class="font-medium text-neutral-600">
 								People who can read this entry ({shared_to ? shared_to.length + 1 : 1})
 							</span>
-							<div class="ppl-scroll max-h-64 min-h-[75px] w-full overflow-y-auto">
+							<div class="ppl-scroll scrollba h-64 min-h-[75px] w-full overflow-y-scroll">
 								<div class="flex h-12 w-full items-center rounded-md pl-1 hover:bg-neutral-200">
 									<div class="h-[40px] w-[40px]">
 										<iconify-icon icon="ph:user-circle" width="40" class="text-neutral-800" />
@@ -254,8 +278,11 @@
 									</span>
 								</div>
 								{#if shared_to}
-									{#each shared_to as user}
+									{#each shared_to as user (user.email)}
 										<div
+											animate:flip
+											in:fade|local
+											out:fly|local={{ x: 100 }}
 											class="flex h-12 w-full items-center justify-between rounded-md pl-1 hover:bg-neutral-200"
 										>
 											<div class="flex items-center">
@@ -275,6 +302,7 @@
 											<button
 												class="mr-2 flex h-[30px] w-[30px] items-center justify-center rounded-full hover:bg-black hover:bg-opacity-10"
 												on:click={() => {
+													//TODO: change to remove from backend
 													shared_to = shared_to.filter((u) => u.email !== user.email);
 												}}
 											>
@@ -286,7 +314,14 @@
 							</div>
 						</div>
 						<div class="mt-5 flex justify-end">
-							<button class="btn-alt" on:click={() => (isOpen = false)}> Done </button>
+							<button
+								class="btn-alt"
+								on:click={() => {
+									isOpen = false;
+								}}
+							>
+								Done
+							</button>
 						</div>
 					</div>
 				</div>
@@ -323,5 +358,23 @@
 
 	.ppl-scroll:hover::-webkit-scrollbar-thumb {
 		border: 6px solid transparent;
+	}
+
+	.shared {
+		animation: 0.3s splash ease-in;
+	}
+
+	@keyframes splash {
+		0% {
+			box-shadow: 0 0 0 0 rgba(115, 146, 68, 1);
+		}
+
+		50% {
+			box-shadow: 0 0 0 10px rgba(115, 146, 68, 0.5);
+		}
+
+		100% {
+			box-shadow: 0 0 0 20px rgba(115, 146, 68, 0);
+		}
 	}
 </style>
