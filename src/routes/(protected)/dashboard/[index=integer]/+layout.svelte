caf7bb3c255d1b47ce64f29d9887b36e7e7b2f79
matthewdicataldo <matthewdicataldo@gmail.com>
2023-02-02 02:22:39 -0700

    scroll bar styling for dashboard editor

--- src/routes/(protected)/dashboard/[index=integer]/+layout.svelte
+++ src/routes/(protected)/dashboard/[index=integer]/+layout.svelte
@@ -2,18 +2,6 @@
 	import { goto } from '$app/navigation';
 </script>
 
-<!-- backdrop-blur-sm -->
-<!-- <div
-	class="fixed top-0 left-0 z-10 h-screen w-screen cursor-pointer overflow-y-auto bg-neutral-500 bg-opacity-80"
->
-	<div
-		class="pointer-events-auto relative mx-auto w-full max-w-screen-lg cursor-auto"
-		use:clickOutside
-		on:click_outside={handleClickOutside}
-	>
-		<slot />
-	</div>
-</div> -->
 <div class="pointer-events-none fixed inset-0 flex h-screen w-screen justify-center ">
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<div
@@ -30,3 +18,27 @@
 		</div>
 	</div>
 </div>
+
+<style lang="postcss">
+	.scroll::-webkit-scrollbar {
+		height: 20px;
+	}
+
+	.scroll::-webkit-scrollbar-track {
+		background-color: transparent;
+		margin-left: 9vw;
+		margin-right: 9.5vw;
+		margin-left: 2rem;
+		margin-right: 2rem;
+		margin-top: 130px;
+	}
+
+	.scroll::-webkit-scrollbar-thumb {
+		border-radius: 20px;
+		border: 5px solid transparent;
+		background-clip: content-box;
+		background-color: rgba(0, 0, 0, 0);
+		background-color: white;
+		transition: 2s;
+	}
+</style>
