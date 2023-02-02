bd0f6edab1a6094153e4fc7b3408f3cf61a0a7fb
ethanjped <eped360@gmail.com>
2023-02-02 15:18:00 -0700

    uhhhh

--- src/routes/(protected)/dashboard/+layout.server.ts
+++ src/routes/(protected)/dashboard/+layout.server.ts
@@ -1,73 +1,99 @@
 import type { LayoutServerLoad } from './$types';
-import { PrismaClient, type JournalEntry } from '@prisma/client';
-const prisma = new PrismaClient()
-
+import { PrismaClient, type Habit, type JournalEntry, type SharedHabit } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export const load = (async ({ locals }) => {
-    const { session, user } = await locals.validateUser();
-    if (!session) return { error: 401, message: "Unauthorized" };
-    if (user) {
-        interface Shared {
-            email: string
-        }
+	const { session, user } = await locals.validateUser();
+	if (!session) return { error: 401, message: 'Unauthorized' };
+	if (user) {
+		interface Shared {
+			email: string;
+		}
 
-        interface JournalEntryWithShared extends JournalEntry {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            SharedEntry: any[],
-            shared?: Shared[],
-        }
+		interface JournalEntryWithShared extends JournalEntry {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			SharedEntry: any[];
+			shared?: Shared[];
+		}
 
-        const post_by_user: JournalEntryWithShared[] | null = await prisma.journalEntry.findMany({
-            where: {
-                user_id: user.userId,
-            },
-            include: {
-                SharedEntry: {
-                    include: {
-                        user: true
-                    }
-                }
-            }
-        });
+		interface HabitWithShared extends Habit {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			SharedHabit: any[];
+			shared?: Shared[];
+		}
 
-        const journal_entries = post_by_user.map(entry => {
-            const { SharedEntry, ...rest } = entry;
+		const post_by_user: JournalEntryWithShared[] | null = await prisma.journalEntry.findMany({
+			where: {
+				user_id: user.userId
+			},
+			include: {
+				SharedEntry: {
+					include: {
+						user: true
+					}
+				}
+			}
+		});
 
-            rest as JournalEntryWithShared;
+		const journal_entries = post_by_user.map((entry) => {
+			const { SharedEntry, ...rest } = entry;
 
-            if (SharedEntry.length > 0) {
-                rest.shared = SharedEntry.map(shared => {
-                    return {
-                        email: shared.user.email as string,
-                    }
-                });
-            } else {
-                rest.shared = [];
-            }
+			rest as JournalEntryWithShared;
 
-            return rest
-        });
+			if (SharedEntry.length > 0) {
+				rest.shared = SharedEntry.map((shared) => {
+					return {
+						email: shared.user.email as string
+					};
+				});
+			} else {
+				rest.shared = [];
+			}
 
-        const habits = await prisma.habit.findMany({
-            orderBy: {
-                id: 'desc'
-            },
-            include: {
-                HabitEntry: {
-                    where: {
-                        date: {
-                            lte: new Date(),
-                            gte: new Date(new Date().setDate(new Date().getDate() - 7))
-                        }
-                    }
-                }
-            }
-        });
+			return rest;
+		});
 
-        return {
-            entries: journal_entries,
-            habits: habits
-        };
-    }
-    return { error: 401, message: "Unauthorized" };
+		const shared_habits: HabitWithShared[] | null = await prisma.habit.findMany({
+			where: {
+				user_id: user.userId
+			},
+			include: {
+				SharedHabit: {
+					include: {
+						user: true
+					}
+				},
+				HabitEntry: {
+					where: {
+						date: {
+							lte: new Date(),
+							gte: new Date(new Date().setDate(new Date().getDate() - 7))
+						}
+					}
+				}
+			}
+		});
+
+		const habits = await prisma.habit.findMany({
+			orderBy: {
+				id: 'desc'
+			},
+			include: {
+				HabitEntry: {
+					where: {
+						date: {
+							lte: new Date(),
+							gte: new Date(new Date().setDate(new Date().getDate() - 7))
+						}
+					}
+				}
+			}
+		});
+
+		return {
+			entries: journal_entries,
+			habits: habits
+		};
+	}
+	return { error: 401, message: 'Unauthorized' };
 }) satisfies LayoutServerLoad;
