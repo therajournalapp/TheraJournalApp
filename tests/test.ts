7fa8e1da67d9bfaf58fbb42c245ae29cf1bb20dc
Cole Taylor <coletaylor017@gmail.com>
2023-02-14 13:19:14 -0700

    Update playwright version to 1.29

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,17 @@
 import { expect, test } from '@playwright/test';
+import config from '../playwright.config.js';
+
+let baseUrl = 'http://localhost:5173';
+if (config.webServer) {
+	baseUrl = `http://localhost:${config.webServer.port}`;
+}
 
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
 	expect(await page.textContent('h1')).toBe('About this app');
+});
+
+test('Accessing dashboard unauthenticated kicks to homepage', async ({ page }) => {
+	await page.goto('/dashboard');
+	expect(page.url()).toBe(`${baseUrl}/`);
 });
