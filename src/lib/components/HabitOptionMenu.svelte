c7dcedbe4119cdec700959cc8be9ebec9dfa1fef
matthewdicataldo <matthewdicataldo@gmail.com>
2023-02-23 14:50:12 -0700

    change text in habit option menu

--- src/lib/components/HabitOptionMenu.svelte
+++ src/lib/components/HabitOptionMenu.svelte
@@ -33,7 +33,8 @@
 								class="group flex w-full items-center rounded-md p-2 text-sm text-gray-900"
 								class:active
 							>
-								<iconify-icon inline icon="ph:trash" class="mr-2 h-5 w-5 text-[20px]" /> Delete Entry
+								<iconify-icon inline icon="ph:trash" class="mr-2 h-5 w-5 text-[20px]" /> Delete Habit
+								Tracker
 							</button>
 						</MenuItem>
 					</div>
