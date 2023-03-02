8f384e007ebc58a50cd48d0d675ef1c155c3fe67
Ethan Pedersen <eped360@gmail.com>
2023-03-01 17:25:01 -0700

    Delete profile working
    Co-authored-by: matthewdicataldo <matthewdicataldo@users.noreply.github.com>

--- src/routes/(container)/(protected)/settings/+page.svelte
+++ src/routes/(container)/(protected)/settings/+page.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 	import { signOut, getUser } from '@lucia-auth/sveltekit/client';
-	import {
-		getAuth,
-		signOut as signOutFirebase,
-		deleteUser,
-		EmailAuthProvider,
-		reauthenticateWithCredential
-	} from 'firebase/auth';
+	import { signIn } from '$lib/firebase/client';
+	import { getAuth, signOut as signOutFirebase } from 'firebase/auth';
 	import { invalidateAll } from '$app/navigation';
 	import Dialog from '$lib/components/Dialog.svelte';
 	import { onMount } from 'svelte';
-	import { redirect } from '@sveltejs/kit';
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
 	const user = getUser();
+	const email = $user?.email;
+	console.log(email); //TODO remove after test
 	const auth = getAuth();
 
 	let confirmDownload = false;
@@ -20,49 +19,45 @@
 	let deleteDialog: HTMLDialogElement;
 	let confirmedPassword = '';
 
+	// Used to export form data to server
+	export let form: ActionData;
+
 	onMount(async () => {
 		console.log(auth);
 	});
 
 	function onConfirmPwordChange(e: Event) {
 		confirmedPassword = (e.target as HTMLInputElement).value;
-		disabledConfirmDelete = confirmedPassword.length < 6;
+		disabledConfirmDelete = confirmedPassword == '';
 	}
 
 	async function onConfirmDelete() {
-		//TODO
-		// first confirm password, login with firebase
-		const userprovidedPassword = confirmedPassword;
-		let email = '';
-		if ($user && !$user.email_verified) {
-			email = $user.email;
-		}
-		const credential = EmailAuthProvider.credential(email, confirmedPassword);
-		if (auth.currentUser != null) {
-			try {
-				let currentUser = auth.currentUser;
-				await reauthenticateWithCredential(currentUser, credential);
-				// User re-authenticated.
-				// then delete user
-				await deleteUser(currentUser);
-				// User deleted in firebase
-				// then delete user in db and lucia
-				await fetch('/api/deleteAccount', {
-					method: 'DELETE',
-					body: JSON.stringify({
-						userId: $user?.userId
-					})
-				});
-				// User deleted in db
-				// then sign out
-				await signOutFirebase(auth);
-				// Sign-out successful.
-				redirect(302, '/login');
-				invalidateAll();
-			} catch (error) {
-				// An error ocurred
-			}
-		} else invalidateAll(); //bad error if current user can't be retrieved, just boot from page
+		// const credential = EmailAuthProvider.credential(email, confirmedPassword);
+		// if (auth.currentUser != null) {
+		// 	try {
+		// 		let currentUser = auth.currentUser;
+		// 		await reauthenticateWithCredential(currentUser, credential);
+		// 		// User re-authenticated.
+		// 		// then delete user
+		// 		await deleteUser(currentUser);
+		// 		// User deleted in firebase
+		// 		// then delete user in db and lucia
+		// 		await fetch('/api/deleteAccount', {
+		// 			method: 'DELETE',
+		// 			body: JSON.stringify({
+		// 				userId: $user?.userId
+		// 			})
+		// 		});
+		// 		// User deleted in db
+		// 		// then sign out
+		// 		await signOutFirebase(auth);
+		// 		// Sign-out successful.
+		// 		redirect(302, '/login');
+		// 		invalidateAll();
+		// 	} catch (error) {
+		// 		// An error ocurred
+		// 	}
+		// } else invalidateAll(); //bad error if current user can't be retrieved, just boot from page
 	}
 </script>
 
@@ -134,35 +129,67 @@
 		</button>
 	</div>
 	<Dialog bind:dialog={deleteDialog} on:close={() => console.log('closed')}>
-		<h1 class="text-xl text-red-600 ">Are you sure you want to delete your account?</h1>
-		<p class="my-4">
-			This will delete all your data, including all journals. You will not be able to recover it.
-		</p>
-		<label class="block text-sm text-gray-700" for="deleteConfirmPword"
-			>Please confirm your password before you delete your account.</label
+		<form
+			method="POST"
+			use:enhance={async ({ form, data, action, cancel }) => {
+				// `data` is its `FormData` object
+				// `action` is the URL to which the form is posted
+				// `cancel()` will prevent the submission
+				const userprovidedPassword = confirmedPassword;
+
+				// Logged in user should always have email
+				if (!email) {
+					cancel();
+					return;
+				}
+
+				const userCredential = await signIn(email, confirmedPassword);
+				const token = await userCredential.user.getIdToken();
+				const user = userCredential.user;
+
+				data.append('token', token);
+				return async ({ result, update }) => {
+					// `result` is an `ActionResult` object
+					// `update` is a function which triggers the logic that would be triggered if this callback wasn't set
+					update();
+				};
+			}}
 		>
-		<input
-			id="deleteConfirmPword"
-			class="input"
-			placeholder="Confirm your password"
-			value={confirmedPassword}
-		/>
-		<hr />
-		<div class="mt-4 flex flex-row justify-end space-x-2">
-			<div class=" flex-col">
-				<button class="btn-gray" on:click={() => deleteDialog.close()}>Cancel</button>
+			<h1 class="text-xl text-red-600 ">Are you sure you want to delete your account?</h1>
+			<p class="my-4">
+				This will delete all your data, including all journals. You will not be able to recover it.
+			</p>
+			<label class="block text-sm text-gray-700" for="deleteConfirmPword"
+				>Please confirm your password before you delete your account.</label
+			>
+			<input
+				id="password"
+				name="password"
+				class="input"
+				placeholder="Confirm your password"
+				on:input={onConfirmPwordChange}
+				value={confirmedPassword}
+			/>
+			<hr />
+			{#if form?.error}
+				<p class="mt-4 text-right text-red-500">{form.error}</p>
+			{/if}
+			<div class="mt-4 flex flex-row justify-end space-x-2">
+				<div class=" flex-col">
+					<button class="btn-gray" on:click={() => deleteDialog.close()}>Cancel</button>
+				</div>
+				<div class="flex-col">
+					<button
+						class="btn-danger"
+						disabled={disabledConfirmDelete}
+						on:click={() => {
+							deleteDialog.close();
+							// onConfirmDelete();
+						}}>Yes, delete everything</button
+					>
+				</div>
 			</div>
-			<div class="flex-col">
-				<button
-					class="btn-danger"
-					disabled={disabledConfirmDelete}
-					on:click={() => {
-						deleteDialog.close();
-						onConfirmDelete();
-					}}>Yes, delete everything</button
-				>
-			</div>
-		</div>
+		</form>
 	</Dialog>
 </div>
 <div class="mt-2 flex flex-row">
