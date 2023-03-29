1fafb940831b37cf0783ec9dd5e954951da8235c
Cole Taylor <coletaylor017@gmail.com>
2023-03-29 17:11:00 -0600

    Remove unused export

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,3 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
 import { defineConfig, devices } from '@playwright/test';
 
 // Any file inside src/tests that contains '[auth]' will be run in an authenticated browser state.
