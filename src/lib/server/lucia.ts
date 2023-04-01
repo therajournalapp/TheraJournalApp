c196e9387e9fde578cd7bf4e007d35055bbd7996
Cole Taylor <coletaylor017@gmail.com>
2023-04-01 12:55:43 -0600

    Fix Lucia.ts file

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -1,14 +1,13 @@
 import lucia, { type User } from 'lucia-auth';
 import prisma from '@lucia-auth/adapter-prisma';
 import { PrismaClient } from '@prisma/client';
-// import { dev } from "$app/environment";
+import { dev } from '$app/environment';
 
 const client = new PrismaClient();
 
 export const auth = lucia({
 	adapter: prisma(client),
-	// env: dev ? "DEV" : "PROD",
-	env: 'DEV',
+	env: dev ? 'DEV' : 'PROD',
 	transformUserData: (userData) => {
 		return {
 			userId: userData.id,
