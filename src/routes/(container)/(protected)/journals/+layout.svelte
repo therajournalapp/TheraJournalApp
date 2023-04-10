e961c7da93a1b6cf2b0989d0c74d4d04f1bf07f1
Ethan Pedersen <eped360@gmail.com>
2023-04-10 15:38:41 -0600

    search filters

--- src/routes/(container)/(protected)/journals/+layout.svelte
+++ src/routes/(container)/(protected)/journals/+layout.svelte
@@ -4,6 +4,7 @@
 	import { createSearchStore, searchHandler } from '$lib/stores/search';
 	import type { JournalEntry } from '@prisma/client';
 	import { onDestroy } from 'svelte';
+	import StandardToggle from '$lib/components/StandardToggle.svelte';
 
 	export let data: PageData;
 
@@ -42,14 +43,40 @@
 		</p>
 	</div>
 {:else}
-	<div class="container mx-auto mb-8 dark:text-neutral-200">
-		<h1 class="label text-lg">Search for journal entries:</h1>
+	<div class="container mx-auto mb-8 rounded-lg p-5 dark:text-neutral-200">
+		<!-- <StandardToggle id="cardview" name="cardview" value="true" /> -->
+		<!-- <span class="float-right">Card View</span> -->
+		<h1 class="label text-lg">Search for your journals:</h1>
 		<input
 			type="text"
 			class="input"
 			bind:value={$searchStore.search}
 			placeholder="Type keywords in journal titles or tags"
 		/>
+		<div class="mt-4 flex items-center">
+			<p class="mr-3 font-medium">Filters</p>
+
+			<button class="btn btn-alt"> Reset Filters </button>
+		</div>
+
+		<div>
+			<div class="mt-4 grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4">
+				<select
+					class="w-full rounded-md border-r-4 border-transparent bg-white py-3 pl-4 text-sm shadow-md shadow-offwhite-light ring-1 ring-black ring-opacity-10 focus:border-gray-500 focus:ring-0 dark:bg-neutral-700 dark:shadow-neutral-800 dark:ring-neutral-700"
+				>
+					<option value="">Last Month</option>
+					<option value="200">Last Year</option>
+					<option value="400">All Time</option>
+				</select>
+				<select
+					class="w-full rounded-md border-r-4 border-transparent bg-white py-3 pl-4 text-sm shadow-md shadow-offwhite-light ring-1 ring-black ring-opacity-10 focus:border-gray-500 focus:ring-0 dark:bg-neutral-700 dark:shadow-neutral-800 dark:ring-neutral-700"
+				>
+					<option value="">Private / Shared</option>
+					<option value="200">Private Only</option>
+					<option value="400">Shared Only</option>
+				</select>
+			</div>
+		</div>
 	</div>
 	<div class="mb-2 flex flex-row flex-wrap justify-start gap-4 self-center ">
 		{#each $searchStore.filtered as entry}
