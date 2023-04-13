ae8f7e347a82c9a47644903436c764d5f9a31d09
matthewdicataldo <matthewdicataldo@gmail.com>
2023-04-12 22:56:27 -0600

    remove min width on usercard

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -8,7 +8,7 @@
 
 <a href="/user/{user_id}" class="text-lg font-medium" class:text-neutral-400={email == ''}>
 	<div
-		class="relative mb-4 flex h-28 min-w-[16rem] max-w-screen-lg flex-col overflow-hidden rounded-lg bg-white p-4 shadow-md shadow-offwhite-light ring-1 ring-black ring-opacity-10 dark:bg-neutral-700 dark:shadow-neutral-800 dark:ring-neutral-700"
+		class="relative mb-4 flex h-28 max-w-screen-lg flex-col overflow-hidden rounded-lg bg-white p-4 shadow-md shadow-offwhite-light ring-1 ring-black ring-opacity-10 dark:bg-neutral-700 dark:shadow-neutral-800 dark:ring-neutral-700"
 	>
 		<div class="flex h-full items-center justify-between gap-3">
 			<UserInitial {email} big />
