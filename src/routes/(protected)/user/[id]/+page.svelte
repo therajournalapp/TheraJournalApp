378e260a587adf380a279453d66c2c4237c5559c
Cole Taylor <coletaylor017@gmail.com>
2023-04-12 20:16:48 -0600

    Mood entry chart works

--- src/routes/(protected)/user/[id]/+page.svelte
+++ src/routes/(protected)/user/[id]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Line } from 'svelte-chartjs';
-	import { chartData } from './data.js';
+	// import { chartData } from './data.js';
 
 	import {
 		Chart as ChartJS,
@@ -17,6 +17,8 @@
 
 	export let data: any;
 
+	let chartData = {};
+
 	async function getMoodEntries() {
 		// Adds the shared user id as a parameter
 		const search_params = new URLSearchParams([['sharedId', data.shared_user_id]]);
@@ -29,18 +31,57 @@
 			}
 		});
 
-		const mood_entries = await res.json();
+		const responseData = await res.json();
 
-		console.log('Retrieved mood entries');
-		console.log(mood_entries);
+		if (responseData.entries) {
+			return responseData.entries;
+		}
+
+		return [];
+	}
+
+	async function populateChartData() {
+		const moodEntries: any = await getMoodEntries();
+
+		console.log('Mood entries: ');
+		console.log(moodEntries);
+
+		chartData = {
+			labels: moodEntries?.map((entry) => entry.date),
+			datasets: [
+				{
+					label: 'My First dataset',
+					fill: true,
+					lineTension: 0.4,
+					backgroundColor: 'rgba(225, 204,230, .3)',
+					borderColor: 'rgb(205, 130, 158)',
+					borderCapStyle: 'butt',
+					borderDash: [],
+					borderDashOffset: 0.0,
+					borderJoinStyle: 'miter',
+					pointBorderColor: 'rgb(205, 130,1 58)',
+					pointBackgroundColor: 'rgb(255, 255, 255)',
+					pointBorderWidth: 10,
+					pointHoverRadius: 5,
+					pointHoverBackgroundColor: 'rgb(0, 0, 0)',
+					pointHoverBorderColor: 'rgba(220, 220, 220,1)',
+					pointHoverBorderWidth: 2,
+					pointRadius: 1,
+					pointHitRadius: 10,
+					data: moodEntries?.map((entry) => entry.value)
+				}
+			]
+		};
 	}
 </script>
 
-{getMoodEntries()}
-
 <h1>Hello from user</h1>
 
-<Line data={chartData} options={{ responsive: false }} width={600} height={300} />
+{populateChartData()}
+
+{#key chartData}
+	<Line data={chartData} options={{ responsive: false }} width={600} height={300} />
+{/key}
 
 <!-- <Line
 	data={{
